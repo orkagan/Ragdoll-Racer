2c0bbff24b087c58f30e18f76725d57b7dc87cff
Jonathan Wang <arachnophobicspyder@gmail.com>
2023-06-11 02:13:25 +1000

    Work on level also added a blast cooldown

--- Assets/Scripts/BlastClickTest.cs
+++ Assets/Scripts/BlastClickTest.cs
@@ -5,12 +5,21 @@ using UnityEngine;
 public class BlastClickTest : MonoBehaviour
 {
     public GameObject blastPrefab;
+    public float cooldownSeconds = 0.5f;
+
+    float _cooldownTime;
     Vector3 offset;
+    
+    private void Start()
+    {
+        _cooldownTime = 0;
+    }
 
     void Update()
     {
+        if (MenuHandler.paused) return;
 
-        if (Input.GetMouseButtonDown(0) & !MenuHandler.paused)
+        if (Input.GetMouseButtonDown(0) & _cooldownTime<=0)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
@@ -18,7 +27,9 @@ public class BlastClickTest : MonoBehaviour
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
                 Destroy(Instantiate(blastPrefab, hit.point, blastPrefab.transform.rotation),2f);
+                _cooldownTime = cooldownSeconds;
             }
         }
+        _cooldownTime = Mathf.Clamp(_cooldownTime - Time.deltaTime, 0, cooldownSeconds);
     }
 }
