a76a3eadd23e8da52bc3bcdf5c1cfadeeef156e9
Jonathan Wang <arachnophobicspyder@gmail.com>
2023-06-12 22:35:10 +1000

    Menu reworks and added goal

--- Assets/Scripts/BlastClickTest.cs
+++ Assets/Scripts/BlastClickTest.cs
@@ -17,7 +17,7 @@ public class BlastClickTest : MonoBehaviour
 
     void Update()
     {
-        if (MenuHandler.paused) return;
+        if (MenuHandler.Instance.CurrentGameState==GameState.Paused) return;
 
         if (Input.GetMouseButtonDown(0) & _cooldownTime<=0)
         {
--- Assets/Scripts/MenuHandler.cs
+++ Assets/Scripts/MenuHandler.cs
@@ -1,43 +1,135 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
+public enum GameState
+{
+    Menu,
+    Playing,
+    Paused,
+    Win
+}
 public class MenuHandler : MonoBehaviour
 {
-    public static bool paused;
-    public GameObject PausePanel;
-    public GameObject GameHUD;
-
-    void Start()
+    [SerializeField]
+    private GameState gameState;
+    public GameState CurrentGameState
     {
-        paused = false;
-        CheckPaused();
+        get
+        {
+            return gameState;
+        }
+        set
+        {
+            gameState = value;
+            CheckPaused();
+            SwitchPanels(gameState);
+        }
     }
+    //public Dictionary<GameState, GameObject> panels;
+    [Serializable]
+    public struct Element
+    {
+        public GameState panelState;
+        public GameObject GameObj;
+    }
+    public Element[] elements;
+
+    #region Singleton Setup
+    //Staticly typed property setup for EnemySpawner.Instance
+    private static MenuHandler _instance;
+    public static MenuHandler Instance
+    {
+        get => _instance;
+        private set
+        {
+            //check if instance of this class already exists and if so keep orignal existing instance
+            if (_instance == null)
+            {
+                _instance = value;
+            }
+            else if (_instance != value)
+            {
+                Debug.Log($"{nameof(MenuHandler)} instance already exists, destroy the duplicate!");
+                Destroy(value);
+            }
+        }
+    }
+    private void Awake()
+    {
+        Instance = this; //sets the static class instance
+    }
+    #endregion
 
     void Update()
     {
         if (Input.GetButtonDown("Pause"))
         {
-            paused = !paused;
-            CheckPaused();
+            if (CurrentGameState==GameState.Playing)
+            {
+                CurrentGameState = GameState.Paused;
+            }
+            else if (CurrentGameState == GameState.Paused)
+            {
+                CurrentGameState = GameState.Playing;
+            }
         }
     }
 
     void CheckPaused()
     {
-        if (paused)
+        switch (CurrentGameState)
         {
-            Time.timeScale = 0;
-            PausePanel.SetActive(true);
-            GameHUD.SetActive(false);
-        }
-        else
-        {
-            Time.timeScale = 1;
-            PausePanel.SetActive(false);
-            GameHUD.SetActive(true);
+            case GameState.Menu:
+                break;
+            case GameState.Playing:
+                Time.timeScale = 1;
+                break;
+            case GameState.Paused:
+                Time.timeScale = 0;
+                break;
+            case GameState.Win:
+                Time.timeScale = 0;
+                break;
+            default:
+                break;
         }
     }
+
+    public void Win()
+    {
+        CurrentGameState = GameState.Win;
+    }
+    public void SwitchPanels(GameState state)
+    {
+        foreach (Element item in elements)
+        {
+            if (item.panelState == CurrentGameState)
+            {
+                item.GameObj.SetActive(true);
+            }
+            else
+            {
+                item.GameObj.SetActive(false);
+            }
+        }
+    }
+    public void NextScene()
+    {
+        // Get the current scene
+        Scene currentScene = SceneManager.GetActiveScene();
+        // Load the next scene after the current scene (last scene loops back around to first)
+        SceneManager.LoadScene((currentScene.buildIndex + 1) % SceneManager.sceneCountInBuildSettings);
+    }
+
+    public void Retry()
+    {
+        //reloads current scene
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
 
     public void ExitGame()
     {
